b24b292e089b277992f6057e242582f156ce286e
YouSY123 <11521609@qq.com>
2024-11-21 22:47:33 +0800

    merge branch

---
+++ typeinfer/Prompt.py
@@ -0,0 +1,23 @@
+from GetStructure import GetStructure
+
+
+def GetPrompt(filepath):
+
+    llamaPrompt = '''
+For the following Python code, infer the type of all variables \n
+'''
+
+    SourceCode = ""
+    with open(filepath, 'r') as src:
+        SourceCode = src.read()
+
+    llamaPrompt += SourceCode
+
+    llamaPrompt += '''
+Then fill in all '{'<type>':'<probability>'}'with the type of the variable and the probability of the type in the following json format.\n
+'''
+    
+    structure = GetStructure()
+    llamaPrompt += str(structure.get(filepath))
+
+    return llamaPrompt
--- typeinfer/llamaInfer.py
+++ typeinfer/llamaInfer.py
@@ -1,31 +1,23 @@
 import ollama
-import sys
+import json
+import Prompt
 
-def llamaInfer(filepath: str):
-    llamaPrompt = '''
-For the following Python code, infer the type of all variables, output in json format {variable name : type } \n
-                  '''
+def llamaInfer(filepath : str, outputpath : str):
 
-    with open(filepath, 'r') as src:
-        SourceCode = src.read()
-
-    llamaPrompt += SourceCode
+    llamaPrompt = Prompt.GetPrompt(filepath)
 
     response = ollama.generate(
-        model='llama3.1',
-        prompt=llamaPrompt,
-        format='json',
-        options={'temperature': 0}
+        model = 'llama3.1',
+        prompt= llamaPrompt,
+        format = 'json',
+        options = {'temperature':0}
     )
 
-    return response['response']
+    result = json.loads(response['response'])
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python llamaInfer.py <python_file_path>")
-        sys.exit(1)
-    
-    result = llamaInfer(sys.argv[1])
-    print(result)
+    with open(outputpath, "w") as json_file:
 
+        json.dump(result, json_file, indent=4)
+
+    return result
     
